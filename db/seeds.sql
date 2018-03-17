7b118e2aac57f5f123816e3fff9d2d49de4f13a2
Edgar Castellanos <edgarcast7@gmail.com>
2018-03-16 23:23:37 -0500

    working on orm

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name) VALUES ('Whooper');
+INSERT INTO burgers (burger_name) VALUES ('Cheese burger');
+-- mine works
+
+-- One example I saw 
+-- INSERT INTO burgers(burger_name, devoured, date)
+-- VALUES ("Big Mac", false, "2016-11-05 04:20:00"),
+-- ("Whooper", false, "2016-11-05 04:20:01"),
+-- ("Cheezburger", false, "2016-11-05 04:20:02"),
