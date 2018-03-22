60290a23ede73871b275127637006a2f58914c72
Edgar Castellanos <edgarcast7@gmail.com>
2018-03-21 21:46:26 -0500

    removed repeated lines

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,16 @@
+//    * In the `orm.js` file, create the methods that will execute
+//  the necessary MySQL commands in the controllers. These are the 
+//  methods you will need to use in order to retrieve and store data 
+//  in your database.
+
+//    * `selectAll()`
+//    * `insertOne()`
+//    * `updateOne()`
+
+//  * Export the ORM object in `module.exports`.
+
+//===========================================================================================================================================
+
 var connection = require("../config/connection.js");
 
 function printQuestionMarks(num) {
@@ -9,33 +22,21 @@ function printQuestionMarks(num) {
 
     return arr.toString();
 }
-
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
     var arr = [];
   
     // loop through the keys and push the key/value as a string int arr
     for (var key in ob) {
-      var value = ob[key];
-      // check to skip hidden properties
-      if (Object.hasOwnProperty.call(ob, key)) {
-        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-        if (typeof value === "string" && value.indexOf(" ") >= 0) {
-          value = "'" + value + "'";
-        }
-        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-        // e.g. {sleepy: true} => ["sleepy=true"]
-        arr.push(key + "=" + value);
-      }
+      arr.push(key + "=" + ob[key]);
     }
-    
-  // translate array of strings to a single comma-separated string
-  return arr.toString();
-}
+
+    return arr.toString();
+  }
 
 // Object for all our SQL statement functions.
 var orm = {
-    all: function(tableInput, cb) {
+    selectAll: function(tableInput, cb) {
       var queryString = "SELECT * FROM " + tableInput + ";";
       connection.query(queryString, function(err, result) {
         if (err) {
@@ -65,47 +66,23 @@ var orm = {
           cb(result);
         });
     },
-
-    create: function(table, cols, vals, cb) {
-        var queryString = "INSERT INTO " + table;
+    update: function(table, objColVals, condition, cb) {
+      var queryString = "UPDATE " + table;
   
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
   
-        console.log(queryString);
-  
-        connection.query(queryString, vals, function(err, result) {
-          if (err) {
-            throw err;
-          }
-  
-          cb(result);
-        });
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
     }
-};
-// An example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
-
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
-
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  }
-};
-
+  };
+    
 // Export the orm object for the model (cat.js).
 module.exports = orm;
